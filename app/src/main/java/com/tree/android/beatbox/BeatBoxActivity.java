aa3c7e6a7d55656e76293a00164e0dc43e6f3914
tree <ltb_2014@126.com>
2018-06-05 14:36:46 +0800

    First

---
+++ app/src/main/java/com/tree/android/beatbox/BeatBoxActivity.java
@@ -0,0 +1,16 @@
+package com.tree.android.beatbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_beat_box);
+    }
+}
